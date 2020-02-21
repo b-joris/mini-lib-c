c90c5576804acfdcd45344c29672297229585cc6
Joris <joris.bertaigne@epitech.eu>
2020-02-21 06:29:30 +0100

    add(memeset): adding memeset function

--- src/memset.asm
+++ src/memset.asm
@@ -1,5 +1,17 @@
 global memset
 
 memset:
-    mov rax, 0
+    mov rax, rdi
+    mov rcx, 0
+
+loop:
+    cmp rcx, rdx
+    jz end
+    mov [rax], sil
+    inc rax
+    inc rcx
+    jmp loop
+
+end:
+    mov rax, rdi
     ret
