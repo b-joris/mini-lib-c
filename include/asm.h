ce8adf87f043bb58d3970b65d4f743714a044a25
Joris <joris.bertaigne@epitech.eu>
2020-02-22 13:02:48 +0100

    add(include): adding include file for unittests

---
+++ include/asm.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** ASM_minilibc_2019
+** File description:
+** asm
+*/
+
+#ifndef ASM_H_
+#define ASM_H_
+
+#include <stddef.h>
+
+size_t my_strlen(const char *s);
+
+#endif /* !ASM_H_ */
