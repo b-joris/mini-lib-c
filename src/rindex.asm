c1bc3eb1bc82aba99cd7e08690c9ece833ecb36f
Joris <joris.bertaigne@epitech.eu>
2020-02-22 03:45:30 +0100

    refactor(rindex): refactoring rindex function

--- src/rindex.asm
+++ src/rindex.asm
@@ -1,21 +1,15 @@
 global rindex
 
 rindex:
-    mov r10, 0
+    mov rax, 0
 
 loop:
     cmp byte[rdi], 0
     je end
     cmp byte[rdi], sil
-    je update_found
-    inc rdi
-    jmp loop
-
-update_found:
-    mov r10, rdi
+    cmove rax, rdi
     inc rdi
     jmp loop
 
 end:
-    mov rax, r10
     ret
